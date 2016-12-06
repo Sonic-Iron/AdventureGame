c4ac166f6fe9c29678e76e93433d203a7289543c
Sonic-Iron <sam@thehewittfamily.com>
2016-12-06 19:13:36 +0000

    Add files via upload

---
+++ AdventureGameDefinitiveEdition.py
@@ -0,0 +1,443 @@
+print("Do not put in a false answer for the pack, it will crash")
+print("Use this in termanal")
+
+#will need to use this code later
+#
+#  s = Sound() 
+#  s.read('sound.mp3') 
+#  s.play()
+
+
+#imports
+import random
+import time
+import os,sys
+import shutil
+import os.path
+
+#variables
+win = 0
+level = 0    
+steps = 0
+coins = 10
+xp = 1
+inventory = []
+Num_Iron = 0
+attack_strength = 0
+brought_item = 0
+sword_durability = 10
+collectables = ["squashed monster in a bottle","broken sword"]
+#admin
+itemcouldwin = []
+couldstart = []
+path = "./"
+os.system("color a")
+devmode = True
+
+
+#difficluty settings
+difficulty = input("What is the difficulty? Easy[E], medium[M] or Hard[H]\n")
+if difficulty == "E":
+    energy = 150
+    hunger = 150
+    Work_seconds = 3
+    sleep_seconds = 4
+    monster_attack_rate = 3
+if difficulty == "M":
+    energy = 100
+    hunger = 100
+    Work_seconds = 10
+    sleep_seconds = 10
+    monster_attack_rate = 2
+if difficulty == "H":
+    energy = 50
+    hunger = 50
+    Work_seconds  = 15
+    sleep_seconds = 15
+    monster_attack_rate = 1
+else:
+    energy = 100
+    hunger = 100
+    Work_seconds = 10
+    sleep_seconds = 10
+    monster_attack_rate = 2
+
+
+#setup
+os.system("Title AdventureGame")
+
+if os.path.exists("__pycache__"):
+    shutil.rmtree("__pycache__")
+
+dirs = os.listdir(path)
+
+filelist = list()
+i=0
+for file in dirs:
+    if file == "AdventureGameDefinitiveEdition.py":
+        ""
+    elif file == "files":
+        ""
+    else:
+        filelist.append(i)
+        filelist[i] = file
+        i=i+1
+        print(i,".",file[0:len(file)-3])
+
+
+chooseLibrary = "Which pack do you want? 1-"+str(len(filelist))+" "
+mychoice = input(chooseLibrary)
+if mychoice == "":
+    mychoice = 1
+
+myimport = filelist[int(mychoice)-1]
+lengthoffilename = len(myimport)
+libraryname = myimport[0:lengthoffilename-3]
+
+
+print(libraryname)
+
+myimport = __import__(libraryname)
+
+database = myimport.default_database()
+object_database = myimport.default_object_database()
+monster_database = myimport.default_monster_database()
+
+
+for a in database:
+    if "dependency" in database[a]:
+        for b in database[a]["objects_in_building"]:
+            if b not in itemcouldwin:
+                itemcouldwin.append(b)
+            
+x = random.choice(itemcouldwin)
+win = x
+
+for a in database:
+    if "dependency" in database[a]:
+        ""
+    else:
+        if a not in couldstart:
+            couldstart.append(a)
+
+x = random.randint(0,len(couldstart)-1)
+current_location = couldstart[x]
+
+
+def info():
+    print("you have :\n\t",level,"\tlevels.\n\t",coins,"\tcoins.\n\t",hunger," \thunger.\n\t",energy," \tenergy. \n\t",xp," \txp.\n\t",steps," \tsteps.")
+
+
+def helper():
+        print("You can 'look', 'move', 'health','drop item','pick up', 'inventory','work','sleep")
+        knowabout = input("What would you like to know about?")
+        if knowabout == "look":
+              print("use this to see what is in your environment")
+        elif knowabout == "move":
+              print("use this is move to difference location")
+        elif knowabout == "health":
+              print("use this is see your stats- hunger, coins, etc, etc...")
+        elif knowabout == "drop item":
+              print("to drop an item type ' drop [an item in your inventory]")
+        elif knowabout == "pick up":
+            print("to pick up an item type ' drop [an item in your current location]")
+        elif knowabout == "inventory":
+            print("Your inventory is what you are  carrying, type 'inventory to see it'")
+        elif knowabout == "work":
+            print("You can only work at a business, you can get money to buy food")
+        elif knowabout == "sleep":
+            print("You can sleep at your house, gain energy by doing it, the length of time it takes is based on your difficulty")
+
+os.system("cls")
+
+     
+current_monster = "none"
+print("Welcome to CommandQuest. Type an instructon to move around the map and interact with the game.  If you are not sure what to do, just type 'help' and follow the instructions") 
+print("\nYou are on a mission to find the",win,"in the shortest number of steps, if you beat the time of the highscorer then you will be the new highscorer\n\n")
+input("press ENTER")
+while True:
+    start = time.time()
+    print("You are at the ",current_location)
+    cmd = input("\nEnter a command : ")
+
+    for a in inventory:
+        for b in collectables:
+            if b in a:
+                energy = energy + 5
+                
+    if cmd == "" or cmd == "help":
+        helper()
+
+        
+    if cmd == "health":
+        info()
+
+
+    if energy < 0:
+        wintype = "death"
+        break
+
+
+    if hunger < 0:
+        wintype = "death"
+        break        
+
+    if cmd == "look":
+        print("Your location is ",current_location)
+        print(database[current_location]['description'])
+        if len(database[current_location]['objects_in_building']) <= 0:
+            print("\nThere's nothing else here")
+        else:
+            print("\nYou can see ",database[current_location]['objects_in_building'])
+           
+    if cmd == "move":
+        print("Where would you like to move to?")
+        print(database[current_location]["directions"])
+        new_location = input("")
+        if new_location not in database[current_location]['directions']:
+           print("Can't go there...")
+        else:
+            if 'dependency' in (database[new_location]): 
+                   if database[new_location]['dependency'] not in inventory:
+                     print("You need ",database[new_location]['dependency'],"to go there.") 
+                   else:
+                       x = random.randint(0,monster_attack_rate)
+                       if x == 0:
+                           keeys = list(monster_database.keys())
+                           current_monster = random.choice(keeys)
+                           print("You get attacked by",current_monster,)
+                           print(monster_database[current_monster]["picture"])
+                           what_to_do = input("Do you want to run or attack?")
+                           if what_to_do == "attack":
+                               for item in inventory:
+                                #Add up the attack points for any object that has them
+                                   if "attack" in object_database[item]:
+                                       attack_strength = attack_strength + object_database[item]['attack']
+                               print("Attack stength is ",attack_strength)         
+                               if attack_strength > (monster_database[current_monster]["health"]):                 
+                                   xp = xp + 1  
+                                   print("You defeated the monster")
+                                   energy = energy - monster_database[current_monster]["health"]
+                                   steps += 1
+                                   info()
+                               else:
+                                   energy = energy - monster_database[current_monster]["health"]
+                                   current_location = new_location
+                                   steps += 1
+                                   print("You get defeated by the",current_monster,"and get tired")
+                                   info()
+                           elif what_to_do == "run":
+                              energy = energy - 5
+                              steps = steps + 1
+                              print("You run back to your previous location and get slighly tried")
+                           else:
+                               print("\nYou did not run away or fight the monster so you get defeated by it anyway")
+                               energy = energy - monster_database[current_monster]["health"]
+                                
+                       else:
+                           current_location = new_location
+                           steps += 1
+                           energy = energy -10
+                           info()
+            else:
+                x = random.randint(0,monster_attack_rate)
+                if x == 0:
+                    keeys = list(monster_database.keys())
+                    current_monster = random.choice(keeys)
+                    print("You get attacked by ",current_monster,)
+                    print(monster_database[current_monster]["picture"])
+                    what_to_do = input("Do you want to run or attack?")
+                    if what_to_do == "attack":
+                        attack_strength = 0 
+                        for item in inventory:
+                        #Add up the attack points for any object that has them
+                            if "attack in object_database[item]":
+                                attack_strength = attack_strength + object_database[item]['attack']
+                        print("Attack stength is ",attack_strength)
+                        if attack_strength > (monster_database[current_monster]["health"]):                 
+                            xp = xp + 1  
+                            print("You defeated the monster")
+                            current_location = new_location
+                            steps += 1
+                            info()
+                        else:
+                            energy = energy - monster_database[current_monster]["health"]
+                            current_location = new_location
+                            steps += 1
+                            print("You get defeated by the",current_monster,"and get trired")
+                            info()
+                    elif what_to_do == "run":
+                        energy = energy - 5
+                        steps = steps + 1
+                        print("You run back to your previous location and get slighly tried")
+                    else:
+                        print("\nYou did not run away or fight the monster so you get defeated by it anyway")
+                        energy = energy - monster_database[current_monster]["health"]
+                                
+                else:
+                    current_location = new_location
+                    steps += 1
+                    energy = energy -10
+                    info()
+                
+
+    if cmd[0:8] == "pick up ":
+        if cmd[8:len(cmd)] in database[current_location]["objects_in_building"]:
+            picked_up_item = cmd[8:len(cmd)]
+            if database[current_location]['type'] == "Selling_point":
+                coins = coins - 1 
+                inventory.append(picked_up_item)
+                database[current_location]['objects_in_building'].remove(picked_up_item)
+                print("Your inventory is",inventory)
+                info()
+            else:
+                inventory.append(picked_up_item)
+                database[current_location]['objects_in_building'].remove(picked_up_item)
+                print("Your inventory is",inventory)    
+            if picked_up_item == win:
+                wintype = "winner"
+                break
+
+    if cmd[0:4] == "drop":
+        dropitem = cmd[5:len(cmd)]
+        if dropitem in object_database:
+            if dropitem in inventory:
+                print("You drop the",dropitem)
+                inventory.remove(dropitem)
+                database[current_location]["objects_in_building"].append(dropitem)
+            else:
+                print("That item is not in your inventory!")
+        else:
+            print("That item does not exist!")
+       
+
+    while len(inventory) > 5:
+        print("You have more than 5 items")
+        dropped_item = input("Which item would you like to drop?")
+        if dropped_item not in inventory:
+            print("That item is not in your inventory")
+        else:
+            database[current_location]["objects_in_building"].append(dropped_item)
+            inventory.remove(dropped_item)
+
+    if cmd == "inventory":
+        print(inventory)
+
+
+
+    if database[current_location]["type"] == "Work_point":
+        if cmd == ("work"):
+            print("You work for",Work_seconds,"hours")
+            oldworksecs = Work_seconds
+            while Work_seconds  > 0:
+                print(Work_seconds)
+                Work_seconds = Work_seconds - 1
+                time.sleep(1)
+            print("You worked for 1 coin")
+            coins = coins + 1
+            hunger = hunger - 10
+            Work_seconds = oldworksecs
+            info()
+
+
+
+    if cmd == "sleep":
+        sleepy = sleep_seconds
+        print("You sleep for",sleep_seconds,"hours")
+        while sleepy > 0:
+            print(sleepy)
+            sleepy = sleepy - 1
+            time.sleep(1)
+        print("You slept for",sleep_seconds,"hours and gained 15 energy")
+        energy = energy +15
+        info()
+
+
+
+
+    if cmd == "eat":
+        print(inventory)  
+        eat = input("What would you like to eat?")
+        if eat not in inventory: 
+            print("That item is not in your inventory")
+        else:
+            if object_database[eat]["type"] == "eat":
+                print("You eat it, that was very refreashing")
+                hunger = hunger + 15
+                inventory.remove(eat)
+            else:
+                print("That is not a food\n")
+                
+
+    if cmd == "examine item" and devmode:
+        current_item = input ("Which item would you like to look at?")
+        if current_item not in object_database:
+            print("That item dosn't exist!")
+        else:
+            print(object_database[current_item]["description"])
+            print("It has",object_database[current_item]["attack_points"]," attack points")
+
+
+    if xp > 10:
+        print("You leveled up one level")
+
+    if cmd == "Give Item" and devmode:
+        for c in object_database:
+            print(c)
+        I =input("Which item would you like to have?")
+        if I in object_database:
+            for a in range(3):
+                print("Giving.")
+                time.sleep(0.5)
+                os.system("cls")
+                print("Giving..")
+                time.sleep(0.5)
+                os.system("cls")
+                print("Giving...")
+                time.sleep(0.5)
+                os.system("cls")
+            time.sleep(0.2)
+            inventory.append(I)
+
+    if cmd == "Teleport" and devmode:
+        for a in database:
+            print(a)
+        where = input("Where would you like to teleport to?")
+        if where in database:
+            current_location = where
+
+
+    if cmd == "highscore":
+        readfile=open('./files/leaderboard.txt')
+        lines=readfile.readlines()
+        print("The current highscore is by...\n",(lines[0]),"and their score is...\n",(lines[1]),"seconds,\n if you beat it you get to be the highscorer for this computer \n")
+        readfile.close()
+
+         
+
+if wintype == "death":
+    os.system("cls")
+    print("You died")
+    time.sleep(10)
+if wintype == "winner":
+    os.system("cls")
+    print("\n\n*******************************************************\n\n")
+    print("CONGRATULATIONS, you found the",win," in ",steps,"steps !!!")
+    print("\n\n*******************************************************\n\n")
+    end = time.time()   
+    readfile=open('./files/leaderboard.txt')
+    lines=readfile.readlines()
+    elapsed = end - start
+    if float(elapsed) < float(lines[1]):
+        name = lines[0]
+        time = lines[1]
+        print("You have beaten the highscorer of ",name,", they had a score of ",time,"seconds")
+        name = input("What is your name?")
+        file = open("./files/leaderboard.txt" ,"w")
+        file.write(name + "\n" + str(round(elapsed, 3)))
+        file.close()
+        readfile.close()
+    else:
+        print("Sorry, you did not beat the current highscorer,",(lines[0]))
+    readfile.close()
+    time.sleep(10)
---
+++ DefaultPack.py
@@ -0,0 +1,194 @@
+def default_database():
+    return {
+    "House":{
+         "description":"It's an old house, it's a little drafty...it could be haunted",
+         "objects_in_building" : ["a lantern","pickaxe"],
+         "directions":("Blacksmiths","Business","Pond","Supermarket"),
+         "type":"Home_point"},
+    "Friends House" :{
+                 "description":"Its a modern house full of electronics and computers",
+                 "objects_in_building":["your friend","your friend's computer","a large fridge"],
+                 "directions":("House","Village Hall","Cave"),
+                 "type":"Sleeping_point"},
+    "Pond":{
+        "description":"Its a nice big pond, great for fishing and making wishes!",
+        "objects_in_building":["a fish","coin toss stall"],
+        "directions":("Friends House","Blacksmiths","House"),
+        "type":"General_point"},
+    "Village Hall":{
+                "description":"A nice medieval hall, full of boxes, mabye there is something in them",
+                "objects_in_building":["nothing","a crate"],
+                "directions":("Blacksmiths","Friends House","Cave"),
+                "type":"General_point"},
+    "Blacksmiths":{
+               "location_name" : "Blacksmiths",
+               "description": "You are in an old and deserted building, you hear a slight creaking of rusting iron",
+               "objects_in_building" : ["an elderly blacksmith"],
+               "directions" : ('Village Hall','Pond','House',"Supermarket"),
+               "type":"Buying_point"},
+    "Business":{"location_name":"business",
+            "description":"unlike the rest of the village they will employ you",
+            "objects_in_building":["a front desk","three workers"],
+            "directions":["House","Forest"],
+            "type":"Work_point"},
+    "Supermarket":{"location_name":"Super Market",
+                "description":"you can buy your food here, there is a pineapple in the frozen foods isle.",
+                "objects_in_building":["a ticket","a pineapple","a carrot","a raw chicken","a frozen yogurt","a z-box game","a rotten fish"],
+                "directions":["Blacksmiths","House"],
+                "type":"Selling_point"},
+    "Cave":{"location_name":"Cave",
+            "description":"very cold and quite dusty, you need a lantern to move here.",
+            "objects_in_building":["gold","dust","spider"],
+            "directions":("Village Hall","Friends House"),
+            "dependency":"a lantern",
+            "type":"Mining_point"},
+    "Forest":{"location_name":"Forest",
+              "description":"A haunted forest, you will need a lantern to move here.",
+              "objects_in_building":[],
+              "directions":("Business","Friends House"),
+              "dependency":"a lantern",
+              "type":"General_point"},
+    }
+
+def default_object_database():
+    return {
+    "a leaf":{
+        "attack" : 2,
+        "description" : "a clear leaf from a diamond tree",
+        "type":"attack"},
+    "fingers":{
+        "attack" : 1,
+        "description" : "a bright lantern, made of steel and gold",
+        "type":"attack"},
+    "spear":{
+        "attack" : 3,
+        "description" : "It's a short plastic spear, not the most fierce weapon I've seen.",
+        "type":"attack"},
+    "a lantern":{
+        "description":"casts a bright light around the room",
+        "type":"general"},
+    "a carrot":{
+        "attack":0,
+        "description":"a tastey treat, and its very nutritious!",
+        "type":"eat"},
+    "your freind":{
+        "attack":2,
+        "description":"your loyal best friend!!!",
+        "type":"attack"},
+    "your friend's computer":{
+        "attack":0,
+        "description":"a really fast computer with 9999999TetraBytes of RAM and CPU",
+        "type":"general"},
+    "a large fridge":{
+        "attack":0,
+        "description":"a gleaming white fridge filled with choclate, cake and chocolate cake",
+        "type":"general"},
+    "a fish":{
+        "attack":0,
+        "description":"you can buy lots of nice cuts of fish from here, only 6 weeks off!!!",
+        "type":"general"},
+    "coin toss stall":{
+        "attack":0,
+        "description":"you can waste a coin here is you wish",
+        "type":"general"},
+    "an elderly blacksmith":{
+        "attack":5,
+        "description":"if you are holding him he will fight for you!",
+        "type":"attack"},
+    "a front desk":{
+        "attack":0,
+        "description":"a nice oak desk from IKEA",
+        "type":"general"},
+    "three workers":{
+        "attack":6,
+        "description":"will fight for you with their pens and three ring binders",
+        "type":"attack"},
+    "a pineapple":{
+        "attack":0,
+        "description":"its a pineapple, end of story",
+        "type":"eat"},
+    "a raw chicken":{
+        "attack":0,
+        "description":"yum! yum! NOT, eat this at your own risk",
+        "type":"eat"},
+    "a frozen yogurt":{
+        "attack":0,
+        "description":"It hurts your teeth doesn't it? try to defrost it first",
+        "type":"eat"},
+    "a z-box game":{
+        "attack":0,
+        "description":"A very fun game to play, unforchunately you don't hav a z-box to play it on",
+        "type":"general"},
+    "a rotten fish":{
+        "attack":0,
+        "description":"YUK, a rotten fish, why are you even looking at this?",
+        "type":"eat"},
+    "gold":{
+        "attack":0,
+        "description":"a really rare resource, oh and it wins the game",
+        "type":"general"},
+    "dust":{
+        "attack":0,
+        "description":"don't even think of picking this up!, it has no use DON'T EVEN THINK ABOUUT IT!",
+        "type":"general"},
+    "spider":{
+        "attack":2,
+        "description":"a really tiny spider, it will fight for you against monsters though",
+        "type":"attack"},
+    "nothing":{
+        "attack":99999,
+        "description":"can kill Death itself",
+        "type":"attack"},
+    "pickaxe":{
+        "attack":2,
+        "description":"Use this pickaxe to mine iron to make swords and armour",
+        "type":"gerneral"},
+    "Steel Sword":{
+        "attack":4,
+        "description":"a basic sword made out of 2 iron",
+        "type":"attack"},
+    "squashed monster in a bottle":{
+        "attack":0,
+        "description":"if you put it in your house, you can have a bonus of health each round",
+        "type":"magical"},
+    "broken sword":{
+        "attack":0,
+        "description":"if you put it in your house, you can have a bonus of health each round",
+        "type":"magical"},
+    }
+
+
+
+def default_monster_database():
+    return {
+   "hairy spider":{
+        "name":"Incy wincy spider",
+        "description":"very large and hairy, has lots of poison",
+        "health":5,
+        "picture":":)"},
+   "small spider":{
+        "name":"Incy wincy spider",
+        "description":"Tiny spider, not very harmful, but people scream anyway.",
+        "health":4,
+        "picture":":)"},
+   "really massive miniscule destuctive peaceful undead rabbit":{
+       "name":"Really Massive Miniscule Destuctive Peaceful Undead Rabbit",
+       "description":"terrorizes the other towns and citys with its pointy nose",
+       "health":7,
+       "picture":":)"},
+   "Death":{
+       "name":"Death",
+       "description":"kills you instantly",
+       "health":99998,
+       "picture":":)"},
+   "unspecial monster":{
+       "name":"unspecial monster",
+       "description":"has no special powers,he uses his face",
+       "health":7,
+       "picture":":)"},
+   "the inediable bulk":{
+       "name":"the inediable bulk",
+       "description":"supprisingly you cannot eat it!,run if you have pizza and chips",
+       "health":10,
+       "picture":":)"},
+   }
