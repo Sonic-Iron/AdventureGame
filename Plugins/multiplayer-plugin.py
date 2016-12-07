106961453f7cdda0dc2d7ce01ef26ef4d40c81db
David Juckes <david.a.juckes@tesco.net>
2016-12-07 19:05:26 +0000

    Fixed 'say' in multiplayer plugin

--- Plugins/multiplayer-plugin.py
+++ Plugins/multiplayer-plugin.py
@@ -1,7 +1,8 @@
 def cmd_intercept(cmd):
     if cmd.startswith('say'):
         file = open('multiplayer.txt', 'a')
-        file.append('\nspeech:player1:' + cmd[4:])
+        file.write('\nspeech:player1:' + cmd[4:])
+        file.close()
         return False
     else:
         return True
